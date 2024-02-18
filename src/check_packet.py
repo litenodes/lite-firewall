32c752ef14f432c34f52d7b868248cdf4852b135
yungwine <cyrbatoff@gmail.com>
2024-02-18 21:05:52 +0700

    bugfix

--- src/check_packet.py
+++ src/check_packet.py
@@ -8,5 +8,12 @@ logger = logging.getLogger(__name__)
 def parse_packet(data: bytes):
     import scapy
     c: scapy.layers.inet.IP = IP(data)
-    logger.debug(c.show())
-    return {'payload': bytes(c.payload.payload), 'src': c.src, 'dst': c.dst, 'sport': c.sport, 'dport': c.dport}
+    result = {
+        'payload': bytes(c.payload.payload),
+        'src': c.src,
+        'dst': c.dst,
+        'sport': c.sport,
+        'dport': c.dport
+    }
+    logger.debug(result)
+    return result
